0c4b46eb8a56673e254e91a7152a8a5b4da9be84
Aadd <aadd@a1>
2025-06-14 16:03:42 +0500

    added inits

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+from .config import AppSettings
+from .database_manager import db_manager
---
+++ app/ui/__init__.py
@@ -0,0 +1,6 @@
+from .login_window import LoginWindow
+from .main_window import MainWindow
+from .view_window import ViewWindow
+from .database_setup_window import DatabaseSetupWindow
+from .user_management_window import UserManagementWindow
+from .edit_window import EditWindow
